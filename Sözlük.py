b8352fec78d0527ed6c4aac0c24bef947bcd791c
mircyyfreestyle <mircyy29@gmail.com>
2024-10-18 20:32:13 +0300

    Create Sözlük.py

---
+++ Sözlük.py
@@ -0,0 +1,14 @@
+a = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "PYTHON":"Bir yazılım dili",
+            "GTG":"Gitmem gerek",
+            "IDE":"Yazılımın yapıldığı ortam",
+            }
+b = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+for i in range(5)
+    if b in a.keys():
+        print (a[b])
+    
+    else:
+            print("*-Kelime yok-*")
